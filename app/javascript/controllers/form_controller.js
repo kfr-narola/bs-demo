b1d312f8eb32371025d9eed0df7c6651537be4bb
kfr-narola <c100-89@C100-89s-iMac-Pro.local>
2024-08-06 18:00:01 +0530

    Changes for form validations

---
+++ app/javascript/controllers/form_controller.js
@@ -0,0 +1,51 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ['form'];
+
+  submitForm(event) {
+    let isValid = this.validateForm(this.formTarget);
+
+    if (!isValid) {
+      event.preventDefault();
+    }
+  }
+
+  validateForm() {
+    let isValid = true;
+
+    let requiredFieldSelectors = 'textarea:required, input:required';
+
+    let requiredFields = this.formTarget.querySelectorAll(requiredFieldSelectors);
+
+    requiredFields.forEach((field) => {
+      if (!field.disabled && !field.value.trim()) {
+        field.focus();
+
+        console.log("ERROR:", field.parentElement);
+
+        isValid = false;
+
+        return false;
+      }
+    });
+    
+    // If we already know we're invalid, just return false
+    if (!isValid) {
+      return false;
+    }
+
+    let invalidFields = this.formTarget.querySelectorAll('input:invalid');
+    
+    invalidFields.forEach((field) => {
+      if (!field.disabled) {
+        field.focus();
+        
+        isValid = false;
+      }
+    });
+    
+    return isValid;
+  }
+
+}
